c8b47711a33d201aad13aab52f45718d7cdfdf2e
manish <manish82kumar2001@gmail.com>
2024-12-20 20:41:31 +0530

    create invoice done

---
+++ app/dashboard/invoices/(overview)/loading.tsx
@@ -0,0 +1,9 @@
+import { InvoiceSkeleton } from "@/app/ui/skeletons"
+
+const loading = () => {
+  return (
+    <InvoiceSkeleton/>
+  )
+}
+
+export default loading
--- app/dashboard/invoices/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>invoices page</div>
-  )
-}
-
-export default page
